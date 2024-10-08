03df8fa804a979f630da69b1f775cc6aa0cec90b
emonShimoul <emonshimoul@gmail.com>
2024-10-08 17:41:09 +0600

    completed the menu

--- navbar.php
+++ navbar.php
@@ -11,27 +11,29 @@
 </div>
 <!-- top bar END-->
 
-<div class="nav-bar container d-flex justify-content-between align-items-center">
-    <div class="navbar-left">
-            <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'mainmenu',
-                        'menu_class'     => 'mainmenucontainer',
-                    )
-                );
-            ?>
-    </div>
-    <div class="navbar-right">
-        <?php if(current_theme_supports( "custom-logo" )): ?>
-        <div class="header-logo text-center">
-            <?php the_custom_logo(); ?>
+<div class="nav-bar">
+    <div class="navbar-container d-flex justify-content-between align-items-center">
+        <div class="navbar-left">
+                <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'mainmenu',
+                            'menu_class'     => 'mainmenucontainer',
+                        )
+                    );
+                ?>
         </div>
-        <?php endif; ?>
+        <div class="navbar-right">
+            <?php if(current_theme_supports( "custom-logo" )): ?>
+            <div class="header-logo text-center">
+                <?php the_custom_logo(); ?>
+            </div>
+            <?php endif; ?>
 
-        <!-- <div class="contacts d-flex">
-            <p class="contact-number">+8801711843533</p>
-            <p>lpgactive@gmail.com</p>
-        </div> -->
+            <!-- <div class="contacts d-flex">
+                <p class="contact-number">+8801711843533</p>
+                <p>lpgactive@gmail.com</p>
+            </div> -->
+        </div>
     </div>
 </div>
