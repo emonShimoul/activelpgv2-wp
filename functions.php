8573daea0aaa582026ac9c8bb4fac0a1da9cde49
emonShimoul <emonshimoul@gmail.com>
2024-10-11 01:04:59 +0600

    some additional code

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ function activelpgv2_assets(){
     wp_enqueue_style("activelpgv2", get_stylesheet_uri());
     wp_enqueue_style("bootstrap", "//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css");
 
-    wp_enqueue_script("bootstrap-js", "//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
+    wp_enqueue_script("bootstrap-js", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
 ",null, "5.3.3", true);
 
     // wp_enqueue_script("alpha2-main", get_template_directory_uri("/assets/js/main.js"),null,"0.0.1",true);
