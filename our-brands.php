741bf26c847b9629c89d2003ebf773af32ca6e59
emonShimoul <emonshimoul@gmail.com>
2024-11-19 13:07:14 +0600

    brands added

--- our-brands.php
+++ our-brands.php
@@ -1,13 +1,19 @@
 <div class="container our-brands">
   <h2>Our <span>Brands</span></h2>
    <section class="customer-logos slider mb-5">
-      <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
-      <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
-     
+      <div class="slide"><img src="https://i.ibb.co.com/s6mgphw/ACI-Limited-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/C9pCXzj/Bashundhara-Group-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/0Vdygsf/Bengal-Group-Logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/8zgy5ZT/bepza-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/jbPyKBK/Concord-Group-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/CzymWnx/drug-international-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/dKXgC17/GPH-Ispat-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/smYzTqt/grs-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/vkwctKG/kazi-farm-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/QmSgvK1/LGED.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/64R1tyL/Palmal-Group-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/D8jX79p/paramount-textile-logo.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/RNvDdyc/square.png"></div>
+      <div class="slide"><img src="https://i.ibb.co.com/Fm22xMz/United-group-logo.png"></div>
    </section>
 </div>
