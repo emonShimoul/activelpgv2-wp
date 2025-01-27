43e81d523befc5d048f2e1c2144ad8eac7a30728
emonShimoul <emonshimoul@gmail.com>
2025-01-27 23:53:52 +0600

    added another slider file

---
+++ slider1.php
@@ -0,0 +1,18 @@
+<div class="slider-container rounded-2xl mb-8 mt-24">
+  <div class="slider">
+    <div class="slide">
+      <img src="https://i.ibb.co.com/nf6MC5N/web-slider.png" alt="">
+    </div>
+    <div class="slide">
+      <img src="https://i.ibb.co.com/s9nxRL2/slider-2.png" alt="">
+    </div>
+    <div class="slide">
+      <img src="https://i.ibb.co.com/Yhj44Xb/slider-3.jpg" alt="">
+    </div>
+    <div class="slide">
+      <img src="https://i.ibb.co.com/W2vn16Y/slider-4.jpg" alt="">
+    </div>
+  </div>
+  <button class="prev-btn">❮</button>
+  <button class="next-btn">❯</button>
+</div>
